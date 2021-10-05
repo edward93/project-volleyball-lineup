ef6426ea968c2d31f360ef0b770c7be552fd32c3
edward93 <edward.chopuryan@gmail.com>
2021-10-05 04:00:41 -0400

    makes player circles responsive

--- src/components/Player.Component.tsx
+++ src/components/Player.Component.tsx
@@ -1,10 +1,13 @@
 import { player } from "../models/player";
+import "../styles/playerComponent.scss";
 
 export const PlayerComponent = (props: { player: player | undefined; positionNumber: number }) => {
   const { player, positionNumber } = props;
   return (
     <div className={`pvl-volleyball-player pvl-player-pos-${positionNumber}`} key={positionNumber}>
-      <div className="pvl-player-main-body">{player?.name}</div>
+      <div className="pvl-player-main-body-container">
+        <div className="pvl-player-main-body-content">{player?.name}</div>
+      </div>
       <div className="pvl-player-info">{player?.role ? `${player.role} - ${positionNumber}` : `${positionNumber}`}</div>
     </div>
   );
