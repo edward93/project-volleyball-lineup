7fa613eb995a7c913ce77ea1c55c43cc3cbbebc2
edward93 <edward.chopuryan@gmail.com>
2023-03-16 01:36:23 -0400

    - updates react scripts - replaces node-sass with sass - adds git-info

--- src/components/Footer.Component.tsx
+++ src/components/Footer.Component.tsx
@@ -1,11 +1,14 @@
 import "../styles/footer.scss";
+import GitInfo from "react-git-info/macro";
 
 export const FooterComponent = () => {
+  const gitInfo = GitInfo();
+
   return (
     <div className="pvl-footer-container">
       <div className="pvl-footer-app-info">
         <div className="pvl-footer-app-author">Made by Ed (エヂイ)</div>
-        <div className="pvl-footer-app-version">version {process.env.REACT_APP_VERSION}</div>
+        <div className="pvl-footer-app-version">version {process.env.REACT_APP_VERSION}-{gitInfo.commit.shortHash}</div>
       </div>
     </div>
   );
